7e9f3f4ce1f0739c0ea77f65779be6634117501e
Betrezen88 <michal.s.wyszynski@gmail.com>
2020-01-05 20:10:43 +0100

    Adde class Disease.

--- NeuroshimaHeroManager.pro
+++ NeuroshimaHeroManager.pro
@@ -14,6 +14,7 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 HEADERS += \
+    src/cpp/Disease.h \
     src/cpp/Penalties.h \
     src/cpp/Penalty.h \
     src/cpp/Professions.h \
---
+++ src/cpp/Disease.h
@@ -0,0 +1,42 @@
+﻿#ifndef DISEASE_H
+#define DISEASE_H
+
+#include <QObject>
+
+#include "Symptoms.h"
+
+class Disease: public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name CONSTANT)
+    Q_PROPERTY(QString description READ description CONSTANT)
+    Q_PROPERTY(QString cure READ cure CONSTANT)
+    Q_PROPERTY(QQmlListProperty<Symptom> symptoms READ symptoms)
+public:
+    Disease(QObject *parent = nullptr): QObject(parent) {}
+    Disease(const QString &name,
+           const QString &description,
+           const QString &cure,
+           QObject *parent = nullptr)
+        : QObject(parent),
+          m_name(name),
+          m_description(description),
+          m_cure(cure),
+          m_symptoms(new Symptoms(this))
+    {}
+
+    QString name() const { return m_name; }
+    QString description() const { return m_description; }
+    QString cure() const { return m_cure; }
+    QQmlListProperty<Symptom> symptoms() { return m_symptoms->symptoms(); }
+
+    void appendSymptom(Symptom *symptom) { m_symptoms->appendSymptom(symptom); }
+
+private:
+    QString m_name;
+    QString m_description;
+    QString m_cure;
+    Symptoms *m_symptoms{nullptr};
+};
+
+#endif // DISEASE_H
