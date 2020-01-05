99320ccc591052a2e6d1cef991c2797a1f462ae5
Betrezen88 <michal.s.wyszynski@gmail.com>
2020-01-05 20:12:22 +0100

    Registered new classes to qml.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,12 @@
 #include "src/cpp/Profession.h"
 #include "src/cpp/Specialization.h"
 #include "src/cpp/Specializations.h"
+#include "src/cpp/Disease.h"
+#include "src/cpp/Diseases.h"
+#include "src/cpp/Symptom.h"
+#include "src/cpp/Symptoms.h"
+#include "src/cpp/Penalty.h"
+#include "src/cpp/Penalties.h"
 
 #include <QDebug>
 
@@ -30,6 +36,12 @@ int main(int argc, char *argv[])
     qmlRegisterType<Profession>("neuroshima.data", 1, 0, "Profession");
     qmlRegisterType<Specializations>("neuroshima.data", 1, 0, "Specializations");
     qmlRegisterType<Specialization>("neuroshima.data", 1, 0, "Specialization");
+    qmlRegisterType<Diseases>("neuroshima.data", 1, 0, "Diseases");
+    qmlRegisterType<Disease>("neuroshima.data", 1, 0, "Disease");
+    qmlRegisterType<Symptoms>("neuroshima.data", 1, 0, "Symptoms");
+    qmlRegisterType<Symptom>("neuroshima.data", 1, 0, "Symptom");
+    qmlRegisterType<Penalties>("neuroshima.data", 1, 0, "Penalties");
+    qmlRegisterType<Penalty>("neuroshima.data", 1, 0, "Penalty");
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
